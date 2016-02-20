2cc482a3646df0276e18bf2b98865b62a016339a
Billingsby <mark@markbillingsby.com>
2016-02-20 16:12:12 -0700

    working thumbs. Buggy lightbox

--- app/app.js
+++ app/app.js
@@ -5,7 +5,34 @@ $(function() {
     getRequest(searchTerm);
     
   });
+
+$('#search-results').on('click', '#tube', function() {
+                      $.fancybox({
+                        'padding'        : 0,
+                        'autoScale'      : false,
+                        'transitionIn'   : 'none',
+                        'transitionOut'  : 'none',
+                        'title'          : this.title,
+                        'width'          : 680,
+                        'height'         : 495,
+                        'href'           : this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
+                        'type'           : 'swf',
+                        'swf'            : {
+                        'wmode'              : 'transparent',
+                        'allowfullscreen'    : 'true'
+                        }
+                    });
+                return false;
+            });
 });
+  
+$('#foo').bind('click', function() {
+  alert($(this).text());
+});
+            
+$('#foo').trigger('click');
+
+
 
 // Global Variables
 var limit = 9;
@@ -14,10 +41,10 @@ var grabItems = [];
 function getRequest(searchTerm) {
   var params = {
     part: 'snippet',
-    q: 'bikes',
+    q: searchTerm,
     chart: 'mostPopular',
     maxResults: limit,
-    key: 'AIzaSyCljHHBsg2XfhNuAE_Vc_0X5Z3qZhB6pVA'
+    key:'AIzaSyCljHHBsg2XfhNuAE_Vc_0X5Z3qZhB6pVA'
   };
   url = 'https://www.googleapis.com/youtube/v3/search';
 
@@ -33,14 +60,20 @@ function getRequest(searchTerm) {
 
 
 function getResults(data){
-    for(var i = 0; i<data.length; i++){
-      var title = $("<h3>").append(data.items[i].snippet.title);
+  var html = "";
+    for(var i = 0; i < limit; i++){
+      var title = data[i].snippet.title;
       console.log(title);
-      var thumb = $("<img>").attr("src",data.items[i].snippet.thumbnails.medium.url);
-      var video_id = data.items[i].snippet.resourceId.videoId;
-      var video_url = 'https://www.youtube.com/watch?v='+video_id;
-      var link = $("<a target='_blank'>").attr("href",video_url).append(thumb);
-      var holder = $('.item').append(link,title);
-      $('.search-reults').append(holder);        
-    }        
+      var thumb = data[i].snippet.thumbnails.medium.url;
+      var video_id = data[i].id.videoId;
+      console.log(video_id);
+      var video_url = 'https://www.youtube.com/watch?v=' + video_id;
+      var link = '<a id=\"tube\" href=\"' + video_url + '\"><img src=\"' + thumb + '\"></a>';
+      html += '<div class=\"item\">' + '<h3>' + title + '</h3>' + link + '</div>';  
+
+    }   
+    console.log(html);
+    $('#search-results').html(html);       
   }  
+
+  
